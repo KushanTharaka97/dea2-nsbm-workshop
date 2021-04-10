063b198cdc0797a51f1e8ab4349fc1fb2953eaa5
KushanTharaka97 <tharakakushan@gmail.com>
2021-04-10 09:48:14 +0530

    Initial Commit

---
+++ src/main/java/lk/nsbm/dea2/springworkshop/SpringWorkshopApplication.java
@@ -0,0 +1,13 @@
+package lk.nsbm.dea2.springworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/nsbm/dea2/springworkshop/SpringWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nsbm.dea2.springworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
